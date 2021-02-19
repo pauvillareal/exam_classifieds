db2aa17390f5dfcade842045f05fbc38e6044046
Pauleen Villareal <aspauleen@gmail.com>
2021-02-19 16:38:00 +0400

    Initial Commit

---
+++ Exam_Classifieds/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exam_Classifieds
+//
+//  Created by Pauleen on 2/19/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Exam_ClassifiedsTests/Exam_ClassifiedsTests.swift
@@ -0,0 +1,33 @@
+//
+//  Exam_ClassifiedsTests.swift
+//  Exam_ClassifiedsTests
+//
+//  Created by Pauleen on 2/19/21.
+//
+
+import XCTest
+@testable import Exam_Classifieds
+
+class Exam_ClassifiedsTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
